88a92588c1ba56cdc3410548d2750b17ba7a9da9
Andrei.Rosca <andrei.rosca@codefactorygroup.com>
2021-12-12 20:21:42 +0200

    Added ci

--- med-assist-backend/src/main/java/inc/evil/medassist/user/web/AuthenticationController.java
+++ med-assist-backend/src/main/java/inc/evil/medassist/user/web/AuthenticationController.java
@@ -1,11 +1,9 @@
 package inc.evil.medassist.user.web;
 
 
-import inc.evil.medassist.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
