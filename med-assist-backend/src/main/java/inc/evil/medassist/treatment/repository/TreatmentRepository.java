c49af6040577e32cc153b0dca4c9afe495eaad41
Andrei.Rosca <andrei.rosca@codefactorygroup.com>
2022-02-11 20:19:16 +0200

    bug: Add the possibility to create treatments without any teeth

--- med-assist-backend/src/main/java/inc/evil/medassist/treatment/repository/TreatmentRepository.java
+++ med-assist-backend/src/main/java/inc/evil/medassist/treatment/repository/TreatmentRepository.java
@@ -11,19 +11,19 @@ import java.util.Optional;
 
 @Repository
 public interface TreatmentRepository extends JpaRepository<Treatment, String> {
-    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient join fetch a.teeth")
+    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient left join fetch a.teeth")
     List<Treatment> findTreatmentsWithDoctorsAndPatientsAndTeeth();
 
-    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient join fetch a.teeth where a.id = :id")
+    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient left join fetch a.teeth where a.id = :id")
     Optional<Treatment> findByIdWithDoctorsAndPatientsAndTeeth(@Param("id") String id);
 
-    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient join fetch a.teeth where a.patient.id = :patientId")
+    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient left join fetch a.teeth where a.patient.id = :patientId")
     List<Treatment> findByPatientIdWithDoctorsAndPatientsAndTeeth(@Param("patientId") String id);
 
-    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient join fetch a.teeth where a.doctor.id = :doctorId")
+    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient left join fetch a.teeth where a.doctor.id = :doctorId")
     List<Treatment> findByDoctorIdWithDoctorsAndPatientsAndTeeth(@Param("doctorId") String id);
 
-    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient join fetch a.teeth teeth where teeth.id = :toothId")
+    @Query("select distinct a from Treatment a join fetch a.doctor join fetch a.patient left join fetch a.teeth teeth where teeth.id = :toothId")
     List<Treatment> findByToothIdWithDoctorsAndPatientsAndTeeth(@Param("toothId") String id);
     
 }
