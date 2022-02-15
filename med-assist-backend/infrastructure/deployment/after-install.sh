6181c114a4441c3c89231ca9459087d178b35508
Andrei.Rosca <andrei.rosca@codefactorygroup.com>
2022-02-15 16:25:01 +0200

    bug: Simplify deployment scripts

--- med-assist-backend/infrastructure/deployment/after-install.sh
+++ med-assist-backend/infrastructure/deployment/after-install.sh
@@ -2,19 +2,13 @@
 
 WORKDIR=$(pwd)
 
-cd /opt/codedeploy-agent/deployment-root/e526a29f-8800-4fbd-8b7d-a1ecc8f77936
-export artifactDirName=$(ls -td -- */ | head -n 1)
-export artifactDirPath=$(readlink -f $artifactDirName)
-
 #Backend
-sudo systemctl stop med-assist
 rm /usr/local/bin/med-assist/med-assist-backend/med-assist.jar
-cp $artifactDirPath/deployment-archive/med-assist.jar /usr/local/bin/med-assist/med-assist-backend/med-assist.jar
+cp /home/ubuntu/med-assist.jar /usr/local/bin/med-assist/med-assist-backend/med-assist.jar
 
 #Frontend
-sudo systemctl stop nginx
 sudo rm -rf /var/www/html/*
-cd /var/www/html; sudo tar -xf $artifactDirPath/deployment-archive/fe.tar
+cd /var/www/html; sudo tar -xf /home/ubuntu/fe.tar
 sudo cp -R /var/www/html/med-assist-client/* /var/www/html/
 
 cd $WORKDIR
